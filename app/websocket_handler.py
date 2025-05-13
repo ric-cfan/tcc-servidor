74ff5a48a501929d781cd0d4256d49c068797ba4
ric-cfan <ric_cfan@hotmail.com>
2025-05-12 23:12:33 -0300

    mudando envio de imagem pura para json

--- app/websocket_handler.py
+++ app/websocket_handler.py
@@ -1,4 +1,6 @@
 import asyncio
+import json
+from datetime import datetime
 from fastapi import WebSocket, WebSocketDisconnect
 import logging
 from app.yolo_service import get_person_snapshot_base64
@@ -16,9 +18,18 @@ async def handle_websocket(websocket: WebSocket):
             snapshot_b64 = get_person_snapshot_base64()
             
             if snapshot_b64:
-                logger.info("Pessoa detectada. Enviando snapshot...")
-                await websocket.send_text(snapshot_b64)
-                await asyncio.sleep(5)  # tempo de "trava" entre envios
+                now = datetime.now().astimezone()
+                data = {
+                    "date": now.strftime("%d/%m/%Y"),
+                    "time": now.strftime("%H:%M:%S"),
+                    "timezone": str(now.tzinfo),
+                    "image_base64": snapshot_b64
+                }
+                json_data = json.dumps(data)
+
+                logger.info("Pessoa detectada. Enviando snapshot com metadados...")
+                await websocket.send_text(json_data)
+                await asyncio.sleep(5)
             else:
                 logger.info("Nenhuma pessoa detectada. Tentando novamente...")
                 await asyncio.sleep(1)
